10df640237309c2c29d16d9f8029dfea956ff2b7
njaysoftware <njpadget@outlook.com>
2020-01-27 16:02:33 -0500

    Added the basic framework for the Decisions Component; Changed the title of the page as well

--- src/components/Application/index.js
+++ src/components/Application/index.js
@@ -7,11 +7,13 @@ To provide a css grid layout for the different component. Also, provides a place
  */
 import React from 'react';
 import Styles from './styles';
-
+import CurrenctDecisionsInput from '../CurrentDescionsComponent/index';
 const Application = () => {
   return (
     <div style={Styles.gridContainer}>
-      <div style={Styles.currentDecisionsContainer}>This is block 1</div>
+      <div style={Styles.currentDecisionsContainer}>
+        <CurrenctDecisionsInput title="Price ($)" startValue={30} changeValue={.5}></CurrenctDecisionsInput>
+      </div>
       <div style={Styles.buttonsContainer}>This is block 1</div>
       <div style={Styles.currentOutputsContainer}>This is block 1</div>
       <div style={Styles.industryAverageContainer}>This is block 1</div>
---
+++ src/components/CurrentDescionsComponent/index.js
@@ -0,0 +1,49 @@
+/**
+Author: Nathaniel Padgett
+Date: 01/24/2020
+Copyright: Nathaniel Padgett 2020
+Purpose:
+Purpose is to supply a repeatable component that can be used as a repeat for the different states.
+ */
+import React from 'react';
+import Styles from './styles';
+import {
+  IconButton,
+  ButtonGroup,
+  Chip,
+} from '@material-ui/core';
+// @TODO: Figure out why this does not work as a relative import
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+const CurrentDecisionsInput = (props) => {
+  // props are a title or type, a function to call when value changes, a key, a starting value, and a increment and decrementer
+  const [counter, setCounter] = React.useState(props.startValue);
+  return (
+    <div style={Styles.Container}>
+      <div style={Styles.LabelContainer}>
+        <label>{props.title}</label>
+      </div>
+      <div style={Styles.ChipContainer}>
+        <Chip label={counter}></Chip>
+      </div>
+      <div style={Styles.ButtonContainer}>
+        <ButtonGroup
+          orientation="vertical"
+        >
+          <IconButton
+            onClick={() => setCounter(counter + props.changeValue)}
+          >
+            <KeyboardArrowUpIcon fontSize='small'/>
+          </IconButton>
+          <IconButton
+            onClick={() => setCounter(counter - props.changeValue)}
+          >
+            <KeyboardArrowDownIcon fontSize='small'/>
+          </IconButton>
+        </ButtonGroup>
+      </div>
+    </div>
+  );
+};
+
+export default CurrentDecisionsInput;
